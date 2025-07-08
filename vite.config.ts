c7c216b945241bd0cdd1cc0cf0086641de401ce9
OctusFinancial <secureyourfuture1123@gmail.com>
2025-07-08 15:19:55 -0400

    Update vite.config.ts
    import { defineConfig } from 'vite';
    import react from '@vitejs/plugin-react';
    
    export default defineConfig({
      plugins: [react()]
    });

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    entry: './src/main.jsx', // ← Add this line
-  },
+  plugins: [react()]
 });
