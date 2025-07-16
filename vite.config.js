94da98cfa15756eaffb4d96a8585baeeb6001d7c
OctusFinancial <secureyourfuture1123@gmail.com>
2025-07-16 15:29:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     rollupOptions: {
       input: 'index.html',
     },
   },
   resolve: {
-    extensions: ['.js'], // Focus on JS only
+    extensions: ['.js'], // optional, you can even remove this line
   },
 });
